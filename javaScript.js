f93fefc1c52627e8f8f3f231dab9228368437217
Dustrider <34176847+DustScripts@users.noreply.github.com>
2024-05-14 13:49:09 -0400

    Assignment complete

--- javaScript.js
+++ javaScript.js
@@ -1,24 +1,19 @@
-// work on seeting up a results div that way the entire div can be removed
-// after results are done under the results-container section.
-
 const postResults = document.getElementById("results-container");
 const buttonId = ["rock", "paper", "scissors"];
 const btns = document.querySelectorAll(".btn");
-const resultComputer = document.createElement("div");
-const resultPlayer = document.createElement("div");
-const gameResult = document.createElement("div");
-const tieResult = document.createElement("div");
-
+const resultComputer = document.createElement("p");
+const resultPlayer = document.createElement("p");
+const gameResult = document.createElement("p");
+const gameResults = document.createElement("p");
+gameResults.classList.add("gameResult");
 let playerWin = 0;
 let computerWin = 0;
-rounds = 5; // Number of rounds to play
-let playedRounds = 0;
 
 // build a better function for the button choices
 btns.forEach((button) => {
   // console.log(button);
   button.addEventListener("click", (choice) => {
-    tieResult.textContent = "";
+    gameResults.textContent = "";
     const playerSelection = choice.target.innerText.toLowerCase();
     const computerSelection = getComputerChoice();
 
@@ -33,26 +28,30 @@ btns.forEach((button) => {
     const result = playRound(playerSelection, computerSelection);
 
     if (result.includes("tie!")) {
-      tieResult.classList.add("tieResult");
-      tieResult.textContent = `It's a TIE! no point awarded`;
-      postResults.appendChild(tieResult);
+      gameResults.textContent = `${result}`;
+      postResults.appendChild(gameResults);
     } else if (result.includes("Win")) {
+      gameResults.textContent = `${result}`;
+      postResults.appendChild(gameResults);
       playerWin++;
     } else if (result.includes("Lose")) {
+      gameResults.textContent = `${result}`;
+      postResults.appendChild(gameResults);
       computerWin++;
     }
 
-    console.log(`Player total wins ${playerWin}`);
-    console.log(`Computer total wins ${computerWin}`);
+    // console.log(`Player total wins ${playerWin}`);
+    // console.log(`Computer total wins ${computerWin}`);
 
-    gameResult.textContent = `Player total wins ${playerWin} \n Computer total wins ${computerWin}`;
+    gameResult.textContent = `Player total wins ${playerWin} \r\n`;
+    gameResult.textContent += `Computer total wins ${computerWin}`;
     postResults.appendChild(gameResult);
 
     if (playerWin === 5) {
-      alert(`Game is done Player Wins`);
+      alert(`Player Wins.... Continue for Next Game`);
       gameReset();
     } else if (computerWin === 5) {
-      alert(`Game is done Computer Wins`);
+      alert(`Computer Wins.... Continue for Next Game`);
       gameReset();
     }
   });
@@ -62,56 +61,6 @@ function gameReset() {
   playerWin = 0;
   computerWin = 0;
   postResults.textContent = "";
-}
-
-// create add event listener for each button
-// document.getElementById(buttonId[0]).addEventListener("click", function () {
-//   btn(buttonId[0]);
-// });
-
-// document.getElementById(buttonId[1]).addEventListener("click", function () {
-//   btn(buttonId[1]);
-// });
-
-// document.getElementById(buttonId[2]).addEventListener("click", function () {
-//   btn(buttonId[2]);
-// });
-
-// button function to run when each button is clicked
-function btn(idButton) {
-  const playerSelection = idButton;
-  const computerSelection = getComputerChoice();
-
-  const resultComputer = document.createElement("div");
-  resultComputer.classList.add("resultComputer");
-  resultComputer.textContent = `Computer selected ${computerSelection}`;
-  postResults.appendChild(resultComputer);
-
-  const resultPlayer = document.createElement("div");
-  resultPlayer.classList.add("resultPlayer");
-  resultPlayer.textContent = `Player selected ${playerSelection}`;
-  postResults.appendChild(resultPlayer);
-
-  // const result = playRound(playerSelection, computerSelection);
-  // console.log(result[0]);
-
-  // Game play - set the rounds you want to play
-
-  console.log(`playerWin Starting number is ${playerWin}`);
-  console.log(`computerWin Starting number is ${computerWin}`);
-
-  // Play multiple rounds
-  // for (let round = 0; round < rounds; round++) {
-  // const playerSelection = getPlayerChoice();
-  // const computerSelection = getComputerChoice();
-  console.log("Player's choice: " + playerSelection);
-  console.log("Computer's choice: " + computerSelection);
-
-  // Play the round and update win counts
-
-  console.log(
-    `******************************* Number of played rounds is ${playedRounds}`
-  );
 }
 
 function getComputerChoice() {
@@ -145,42 +94,3 @@ function playRound(playerSelection, computerSelection) {
     return "You Lose! " + computerSelection + " beats " + playerSelection;
   }
 }
-
-// function playGame() {
-//   // Game play - set the rounds you want to play
-//   let playerWin = 0;
-//   let computerWin = 0;
-//   // const rounds = 5; // Number of rounds to play
-
-//   // Play multiple rounds
-//   // for (let round = 0; round < rounds; round++) {
-//   // const playerSelection = getPlayerChoice();
-//   // const computerSelection = getComputerChoice();
-//   console.log("Player's choice: " + playerSelection);
-//   console.log("Computer's choice: " + computerSelection);
-
-//   // Play the round and update win counts
-//   const result = playRound(playerSelection, computerSelection);
-//   console.log(result);
-//   if (result.includes("Win")) {
-//     playerWin++;
-//   } else if (result.includes("Lose")) {
-//     computerWin++;
-//   }
-//   // }
-
-//   // Display the final score
-//   // Later maybe setup some kind of on-going scoreboard
-//   console.log("Player wins: " + playerWin);
-//   console.log("Computer wins: " + computerWin);
-//   if (playerWin == computerWin) {
-//     console.log('IT"S A TIE - NOBODY WINS!');
-//   } else if (playerWin > computerWin) {
-//     console.log("The Player wins the GAME! with " + playerWin);
-//   } else {
-//     console.log("The Computer wins the GAME! with " + computerWin);
-//   }
-// }
-
-// Play the GAME!
-// playGame();
